3295667a06ce83b011e241ab7d9c95ba7bc8ac81
Ugboaja Uchechi <74814780+Ugboaja-Uchechi@users.noreply.github.com>
2022-01-24 15:24:56 +0100

    Fix linter errors

--- person.rb
+++ person.rb
@@ -1,28 +1,21 @@
 class Person
-
   attr_reader :id
-  attr_accessor :name
-  attr_accessor :age
+  attr_accessor :name, :age
 
-  def initialize(name = "Unknown", age, parent_permission: true)
+  def initialize(age, name = 'Unknown', parent_permission: true)
     @id = id
     @name = name
     @age = age
     @parent_permission = parent_permission
   end
 
-  def is_of_age?
-    if @age >= 18
-      true
-    else
-      false
-    end
+  def of_age?
+    @age >= 18
   end
-  private :is_of_age?
 
   def can_use_services?
-    is_of_age? || @parent_permission == true
+    of_age? || @parent_permission == true
   end
 
-
+  private :of_age?
 end
--- teacher.rb
+++ teacher.rb
@@ -1,7 +1,7 @@
-require'./person'
+require './person'
 
 class Teacher < Person
-  def initialize(name = "Unknown", age, specialization, parent_permission: true)
+  def initialize(age, specialization, name = 'Unknown', parent_permission: true)
     super(name, age, parent_permission: parent_permission)
     @specialization = specialization
   end
