f631de4380d8a8cf566ad4bf768b95337cd7e854
Ugboaja Uchechi <74814780+Ugboaja-Uchechi@users.noreply.github.com>
2022-01-24 14:13:43 +0100

    Create student class

--- person.rb
+++ person.rb
@@ -9,7 +9,6 @@ class Person
     @name = name
     @age = age
     @parent_permission = parent_permission
-
   end
 
   def is_of_age?
---
+++ student.rb
@@ -0,0 +1,12 @@
+require'./person'
+
+class Student < Person
+  def initialize(name = "Unknown", age, parent_permission = true, classroom)
+    super(name, age, parent_permission: parent_permission)
+    @classroom = classroom
+  end
+
+  def play_hooky
+    puts "¯\(ツ)/¯"
+  end
+end
