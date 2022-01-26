bf2cabcb625a2fa31ad8060a7e7126684e10ae5d
Ugboaja Uchechi <74814780+Ugboaja-Uchechi@users.noreply.github.com>
2022-01-26 14:56:11 +0100

    Create relationships between Rental, book and person

--- book.rb
+++ book.rb
@@ -1,8 +1,16 @@
+require './rental'
+require './person'
+
 class Book
-  attr_accessor :title, :author
+  attr_accessor :title, :author, :rentals
 
   def initialize(title, author)
     @title = title
     @author = author
+    @rentals = []
+  end
+
+  def add_rental(date, person)
+    Rental.new(date, self, person)
   end
 end
