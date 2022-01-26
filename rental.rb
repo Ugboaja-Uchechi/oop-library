a64cd8536d4cccb516328bb3930aa601234ee15f
Ugboaja Uchechi <74814780+Ugboaja-Uchechi@users.noreply.github.com>
2022-01-26 15:04:02 +0100

    Fix linte error

--- rental.rb
+++ rental.rb
@@ -1,7 +1,7 @@
 class Rental
   attr_accessor :date, :book, :person
 
-  def initialize(date, book, person)
+  def initialize(date, book, _person)
     @date = date
     @book = book
     @book.rentals << self
--- student.rb
+++ student.rb
@@ -1,6 +1,8 @@
 require './person'
 
 class Student < Person
+  attr_reader :classroom
+
   def initialize(age, classroom, name = 'Unknown', parent_permission: true)
     super(name, age, parent_permission: parent_permission)
     @classroom = classroom
@@ -10,7 +12,7 @@ class Student < Person
     puts "¯\(ツ)/¯"
   end
 
-  def classroom = (classroom)
+  def classroom=(classroom)
     @classroom = classroom
     @classroom.students.push(self) unless @classroom.students.include?(self)
   end
