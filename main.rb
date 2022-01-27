d3dff4d37d6e1377854bac2d0622b78bd596d198
Ugboaja Uchechi <74814780+Ugboaja-Uchechi@users.noreply.github.com>
2022-01-27 11:54:45 +0100

    Create person (teacher and student)

--- main.rb
+++ main.rb
@@ -10,6 +10,57 @@ class Ui
     @people = []
   end
 
+  def person
+    puts 'Do you want to create a student (1) or a teacher (2) ?'
+    option = Integer(gets.chomp)
+
+    case option
+    when 1
+      print 'Age: '
+      users_age = Integer(gets.chomp)
+
+      print 'Name: '
+      users_name = gets.chomp
+
+      print 'Has parent(s) permission? [Y/N]: '
+      users_parent_permission = gets.chomp
+
+      case users_parent_permission
+      when 'y'
+        users_parent_permission = true
+
+      when 'n'
+        users_parent_permission = false
+      end
+      student = Student.new(users_age, users_name, parent_permission: users_parent_permission)
+
+      @people.push({
+        output: "[Student] Name: #{student.name}, ID: #{student.id}, Age: #{student.age}",
+        object: student
+      })
+      puts 'Student created Successfully !'
+
+    when 2
+      print 'Age: '
+      users_age = Integer(gets.chomp)
+
+      print 'Name: '
+      users_name = gets.chomp
+
+      print 'Specialization: '
+      users_specialization = gets.chomp
+
+      teacher = Teacher.new(users_age, users_name, users_specialization)
+
+      @people.push({
+        output: "[Teacher] Name: #{teacher.name}, ID: #{teacher.id}, Age: #{teacher.age}",
+        object: teacher
+      })
+      puts 'Teacher created Successfully !'
+    else
+      puts 'Wrong option'
+  end
+
   def list_books
     @books.push do |book|
       puts book [:output]
