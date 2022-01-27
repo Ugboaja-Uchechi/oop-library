79dfaaf257cb94be019d5492468bca77fce19c7f
Ugboaja Uchechi <74814780+Ugboaja-Uchechi@users.noreply.github.com>
2022-01-27 11:10:46 +0100

    Create people list method

--- main.rb
+++ main.rb
@@ -7,11 +7,18 @@ require './rental'
 class Ui
   def initialize
     @books = []
+    @people = []
   end
 
   def list_books
     @books.push do |book|
       puts book [:output]
+    end
+  end
+
+  def list_people
+    @people.push do |person|
+      puts person [:output]
     end
   end
 end
